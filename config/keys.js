55ce014b828aceb191dfe8bf3934ed72c2370dc9
Rohan Keskar <33134458+rohankeskar19@users.noreply.github.com>
2019-06-14 19:17:22 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,1 @@
-module.exports = {
-    "secret" : "aslfkjafsj12123123lksafkl",
-    "mongoDB" : "mongodb://rohan.keskar:Totalfire12345$@ds211088.mlab.com:11088/chatapp",
-    "secretAccesKey" : "ZAke6rUfoM+Dz1sbEnBWbavJZ8l1YEUvn/EnS6CE",
-    "accesKeyId" : "AKIAIE2GPVDWMQMSEIHQ",
-    "region" : "us-east-1",
-    "tokenExpiry" : 60 * 60 * 60 * 5
-}
+
